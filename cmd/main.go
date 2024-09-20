8a00d79f7c2a6b356faf85c546f6118cdfa24f64
viniciusfal <vinicius_prsilva@hotmail.com>
2024-09-20 16:06:12 -0300

    refactor: controllers

--- cmd/main.go
+++ cmd/main.go
@@ -2,31 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/viniciusfal/erp/db"
-	"github.com/viniciusfal/erp/http/controller"
-	"github.com/viniciusfal/erp/infra/repository"
-	"github.com/viniciusfal/erp/infra/usecase"
+	"github.com/viniciusfal/erp/http/routes"
 )
 
 func main() {
-	router := gin.Default()
+	Server := gin.Default()
 
-	dbConnection, err := db.ConnectDB()
-	if err != nil {
-		panic(err)
-	}
-
-	TransactionRepository := repository.NewTransactionRepository(dbConnection)
-	TransactionUseCase := usecase.NewTransactionUseCase(TransactionRepository)
-	TransactionController := controller.NewTransactionController(TransactionUseCase)
-
-	router.GET("/ping", func(ctx *gin.Context) {
+	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	router.POST("/transaction", TransactionController.CreateTransaction)
+	routes.TransactionRoutes()
 
-	router.Run(":8000")
+	server.Run(":8000")
 }
