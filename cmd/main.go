5a8b51da24adab08e7843ce3fe2e7579c333d968
viniciusfal <vinicius_prsilva@hotmail.com>
2024-09-26 22:56:40 -0300

    feature: cors

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/viniciusfal/erp/http/routes"
 )
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8000"},
+		AllowMethods:     []string{"PUT, PATCH, GET, POST, DELETE"},
+		AllowHeaders:     []string{"origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.TransactionRoutes(server)
 	routes.UserRoutes(server)
