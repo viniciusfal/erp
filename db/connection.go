1ab67a9ed16a1f2e9477cfa7be91740ef6c1d2cd
viniciusfal <vinicius_prsilva@hotmail.com>
2024-11-11 11:00:06 -0300

    feat: build var

--- db/connection.go
+++ db/connection.go
@@ -3,33 +3,37 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "1234"
-	dbname   = "postgres"
-)
-
 func ConnectDB() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+	// Usando variáveis de ambiente para configurar a conexão com o banco de dados
+	host := os.Getenv("DB_HOST")         // Por exemplo: "localhost" ou URL do banco no Render
+	port := os.Getenv("DB_PORT")         // Exemplo: "5432"
+	user := os.Getenv("DB_USER")         // Exemplo: "postgres"
+	password := os.Getenv("DB_PASSWORD") // Exemplo: "your_password"
+	dbname := os.Getenv("DB_NAME")       // Exemplo: "your_db_name"
+
+	// Formatar a string de conexão com PostgreSQL
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+
+	// Abre a conexão com o banco de dados
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to open the database: %v", err)
 	}
 
+	// Verifica se a conexão foi bem-sucedida
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to ping the database: %v", err)
 	}
 
-	fmt.Println("Connected to " + dbname)
+	// Log de conexão bem-sucedida
+	fmt.Println("Connected to database:", dbname)
 
 	return db, nil
 }
