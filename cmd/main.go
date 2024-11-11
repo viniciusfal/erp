ee0936a98c2878251ac22386f59bc9400e76310e
viniciusfal <vinicius_prsilva@hotmail.com>
2024-11-11 11:34:41 -0300

    fix: connections

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -11,19 +12,28 @@ import (
 func main() {
 	server := gin.Default()
 
+	// Configuração do CORS para permitir acesso tanto do frontend local quanto da produção
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},                 // Permita apenas esta origem
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"}, // Corrigido
+		AllowOrigins:     []string{"http://localhost:5173", "https://erp-1xqz.onrender.com"}, // Domínios permitidos
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
 
+	// Configuração das rotas
 	routes.TransactionRoutes(server)
 	routes.UserRoutes(server)
 	routes.MetaRoutes(server)
 	routes.SafeRoutes(server)
 
-	server.Run(":8000")
+	// Obter a porta via variável de ambiente para compatibilidade com o Render
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // Porta padrão para desenvolvimento
+	}
+
+	// Rodar o servidor na porta especificada
+	server.Run(":" + port)
 }
