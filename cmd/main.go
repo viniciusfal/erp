e111d90a2b81ef5d7c372a874e0b35a3619eabec
viniciusfal <vinicius_prsilva@hotmail.com>
2024-11-18 11:21:41 -0300

    feat: test new

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/viniciusfal/erp/db"
+	"github.com/viniciusfal/erp/http/routes"
 )
 
 func main() {
+	dbConnection := db.RunDB()
 	server := gin.Default()
 
 	// Configuração do CORS para permitir acesso tanto do frontend local quanto da produção
 	server.Use(cors.New(cors.Config{
-
-		AllowOrigins: []string{"http://localhost:5173", "https://api-erp-amazonia.up.railway.app", "https://erpamazoniainter.vercel.app"}, // Domínios permitidos
-
+		AllowOrigins:     []string{"http://localhost:5173", "https://erp-1xqz.onrender.com", "https://erpamazoniainter.vercel.app"}, // Domínios permitidos
 		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
@@ -23,12 +25,17 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	// Obter a porta via variável de ambiente para compatibilidade com o Render
+	routes.TransactionRoutes(server)
+	routes.MetaRoutes(server)
+	routes.SafeRoutes(server)
+	routes.UserRoutes(server)
+
+	fmt.Printf("Banco de dados conectado com sucesso: %v\n", dbConnection)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000" // Porta padrão para desenvolvimento
 	}
 
-	// Rodar o servidor na porta especificada
 	server.Run(":" + port)
 }
