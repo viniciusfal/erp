7e2fb43fa1e493af561f57b45c086bd41ce2b8e5
viniciusfal <vinicius_prsilva@hotmail.com>
2025-02-06 15:32:32 -0300

    feat: api public

--- cmd/main.go
+++ cmd/main.go
@@ -39,7 +39,7 @@ func main() {
 		routes.SafeRoutes(api)
 	}
 
-	apiPublic := server.Group("/api/ses")
+	apiPublic := server.Group("/api-public")
 	{
 		routes.UserRoutes(apiPublic) // Aqui, a rota de login pode ser acessada sem JWT
 	}
